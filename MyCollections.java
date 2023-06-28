f1b5b5631e4fb67ac175f7117398850993b7c0d2
Dmitrii <73357024+cdelicia@users.noreply.github.com>
2023-06-27 23:53:44 -0700

    Update method names, error handling
    added: error handling if inconsistent invocations
    changed: method names

--- MyCollections.java
+++ MyCollections.java
@@ -1,3 +1,5 @@
+package com.collections;
+
 import java.util.Random;
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -7,36 +9,37 @@ import java.util.Arrays;
 import java.util.TreeSet;
 
 public class MyCollections {
-    private static final int VARIETY = 70;
-    private static final int SIZE = 20;
-    public static final String RED_COLOR = "\033[1;31m";
-    public static final String GREEN_COLOR = "\033[1;32m";
+    private static final String RED_COLOR = "\033[1;31m";
+    private static final String GREEN_COLOR = "\033[1;32m";
     private static final Collection<Integer> hashSet = new HashSet<>();
     private static final List<Integer> arrayList = new ArrayList<>();
     private static final Collection<Integer> treeSet = new TreeSet<>();
 
 
-    public static void main(String[] args) {
-
-        initializationOfHashSetByValues();
-        printResult(hashSet, "Original HashSet in the very beginning", RED_COLOR);
-
-        initializationOfArrayListByRandomValues();
-        printResult(arrayList, "Original ArrayList in the very beginning", RED_COLOR);
-
+    public static void mergeTwoCollections() {
+        if (arrayList.isEmpty() || hashSet.isEmpty()) {
+            System.out.println("Error: First of all call init methods for ArrayList and HashSet\n");
+            return;
+        }
         hashSet.addAll(arrayList);
         printResult(hashSet, "HashSet after merging with ArrayList", RED_COLOR);
         arrayList.clear();
+    }
 
+    public static void sortHashSetAfterMerging() {
+        if (hashSet.size() == Main.SIZE_OF_HASHSET) {
+            System.out.println("Error: Secondly call merge method for merging ArrayList and HashSet\n");
+            return;
+        }
         /**
-         * First solution
+         * First solution via Arrays.class
          */
         Object[] newArray = hashSet.toArray();
         Arrays.sort(newArray);
         printResult(newArray);
 
         /**
-         * Second solution
+         * Second solution via TreeSet.class
          */
         treeSet.addAll(hashSet);
         printResult(treeSet, "Solution #2: TreeSet after instantiation", GREEN_COLOR);
@@ -44,34 +47,40 @@ public class MyCollections {
 
 
     /**
-     * Method Overloading
+     * Overloaded Print Methods
      */
     private static void printResult(Collection<Integer> collection, String comment, String color) {
         System.out.println(color + comment + ", size=" + collection.size() + "\033[0m");
-        for (Integer element : collection)
+        for (Integer element : collection) {
             System.out.print(element + " ");
+        }
         System.out.println('\n');
     }
 
     private static void printResult(Object[] array) {
         System.out.println("\033[1;32mSolution #1: New array after sorting, size=" + array.length + "\033[0m");
-        for (Object i : array)
+        for (Object i : array) {
             System.out.print(i + " ");
+        }
         System.out.println('\n');
     }
 
-
-    private static void initializationOfArrayListByRandomValues() {
+    /**
+     * Collections Initialization Methods
+     */
+    public static void initOfArrayListByRandomValues() {
         Random random = new Random();
 
-        for (int i = 0; i < SIZE; i++) {
-            arrayList.add(random.nextInt(VARIETY));
+        for (int i = 0; i < Main.SIZE_OF_ARRAYLIST; i++) {
+            arrayList.add(random.nextInt(Main.VARIETY_OF_ELEMENTS_IN_ARRAYLIST));
         }
+        printResult(arrayList, "Original ArrayList in the very beginning", RED_COLOR);
     }
 
-    private static void initializationOfHashSetByValues() {
-        for (int i = 95; i > 0; i -= 5) {
+    public static void initOfHashSetByValues() {
+        for (int i = 0; i < Main.SIZE_OF_HASHSET * Main.GAP_FOR_HASHSET; i += Main.GAP_FOR_HASHSET) {
             hashSet.add(i);
         }
+        printResult(hashSet, "Original HashSet in the very beginning", RED_COLOR);
     }
 }
