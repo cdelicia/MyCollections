42caa0b08faa947df8eadb62452cc1f1e0e9d525
Dmitrii <73357024+cdelicia@users.noreply.github.com>
2023-06-29 00:36:44 -0700

    Update Main.java
    added: one more method call (Stream solution)

--- Main.java
+++ Main.java
@@ -10,7 +10,10 @@ public class Main {
 
         MyCollections.initOfHashSetByValues();
         MyCollections.initOfArrayListByRandomValues();
-        MyCollections.mergeTwoCollections();
+
+        MyCollections.mergeAndSortUsingStream();
+
+        MyCollections.mergeTwoCollectionsUsingHashSet();
         MyCollections.sortHashSetAfterMerging();
     }
 }
